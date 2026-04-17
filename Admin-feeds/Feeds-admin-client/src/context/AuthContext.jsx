855b0120c6e2d15058a238984a6bf79706f35c95
VoyagerX21 <khakse2gaurav2003@gmail.com>
2026-04-17 19:16:34 +0000

    typo

--- Admin-feeds/Feeds-admin-client/src/context/AuthContext.jsx
+++ Admin-feeds/Feeds-admin-client/src/context/AuthContext.jsx
@@ -11,7 +11,7 @@ export const AuthProvider = ({ children }) => {
     useEffect(() => {
         const checkAuth = async () => {
             try {
-                const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/status`, { withCredentials: true });
+                const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/status`, { credentials: true });
                 const data = await res.json();
 
                 setIsAuthenticated(true);
