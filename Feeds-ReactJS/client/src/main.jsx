29c41b648584d53dd1bd3bbd94eb31d9b16949f5
VoyagerX21 <khakse2gaurav2003@gmail.com>
2026-04-18 19:32:00 +0000

    file import issue resolved

--- Feeds-ReactJS/client/src/main.jsx
+++ Feeds-ReactJS/client/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
-import ErrorBoundary from './components/ErrorBoundary.jsx';
+import ErrorBoundary from './components/errorBoundary.jsx';
 import ThemeManager from './components/themeManager.jsx';
 import { ErrorProvider } from './providers/ErrorContext.jsx';
 import NotFound from './components/error.jsx';
