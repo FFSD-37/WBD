167eafd0e3f02eea8053b1db0c14af72a8d27a6c
VoyagerX21 <khakse2gaurav2003@gmail.com>
2026-04-19 08:25:19 +0000

    feeds-reactjs cors updated to vercel url

--- Feeds-ReactJS/server/index.js
+++ Feeds-ReactJS/server/index.js
@@ -60,7 +60,7 @@ app.use(cookieParser());
 // ✅ Allow frontend at http://localhost:5173
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://wbd-zzho.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
